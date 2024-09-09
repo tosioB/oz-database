ae3fa0bc2e2358ce1b51697b97ac58be3c18e673
kimdaesik <ds500033@naver.com>
2024-09-09 15:32:04 +0900

    강의 따라하기 재작업 및 수정

---
+++ 4-서버와_데이터베이스_학습.js
@@ -0,0 +1,35 @@
+/** ORM(Object-Relational Mapping)
+ * 객체와 로우를 연결(map)해주는 도구
+ * Node.js와 데이터베이스를 이어주는 도구
+ * JAVA와 데이터베이스를 이어주는 도구
+ */
+
+/** ORM 장점
+ * 생산성, 유지보수 용이
+ * DB 독립적
+ * 보안
+ */
+
+/** Prisma
+ * Node.js로 백엔드 개발할때 가장 쉽게 접근 할 수 있는 ORM중 하나
+ */
+
+/** Prisma 장점
+ * Node.js, TS로 개발 가능
+ * Beginner-friendly
+ * DB 독립성, 마이그레이션
+ */
+
+/** 3티어 장점
+ * 모듈화
+ * 보안성
+ * 확장성
+ */
+
+/** 작성순서 아님
+ * package.json설치 - npm init -y
+ * prisma설치 - npm install prisma -D 
+ * prisma설치 확인 - npx prisma
+ * prisma시작 - npx prisma init
+ * prisma를 스키마로 땡겨온다 - npx prisma db pull
+ */
